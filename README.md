2a65c5764f20e435271ae0765f550a0c4e7da34e
Joel Christner <joel.christner@gmail.com>
2024-01-22 12:53:16 -0800

    NuGet v6.1.7 (Watson.Lite), fix for dead wires, thank you @GitHubProUser67

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ I'd like to extend a special thanks to those that have helped make Watson Webser
 - @notesjor @shdwp @Tutch @GeoffMcGrath @jurkovic-nikola @joreg @Job79 @at1993 @MartyIX 
 - @pocsuka @orinem @deathbull @binozo @panboy75 @iain-cyborn @gamerhost31 @nhaberl 
 - @grgouala @sapurtcomputer30 @winkmichael @sqlnew @SaintedPsycho @Return25 @marcussacana 
-- @samisil @Jump-Suit @ChZhongPengCheng33 @bobaoapae @rodgers-r @john144 @zedle
+- @samisil @Jump-Suit @ChZhongPengCheng33 @bobaoapae @rodgers-r @john144 @zedle @GitHubProUser67
 
 ## Watson vs Watson.Lite
 
--- src/WatsonWebserver.Lite/HttpResponse.cs
+++ src/WatsonWebserver.Lite/HttpResponse.cs
@@ -585,12 +585,11 @@ namespace WatsonWebserver.Lite
                 {
                     await _Stream.WriteAsync(headers, 0, headers.Length, token).ConfigureAwait(false);
                     await _Stream.FlushAsync(token).ConfigureAwait(false);
-                    _HeadersSent = true; // We use the flag at our advantage, so we can effectively check for dead wires.
+                    _HeadersSent = true; 
                 }
                 catch (ObjectDisposedException)
                 {
-                    /* Always check if the stream is disposed, some clients are picky about this and cut the wire in the middle of a request.
-                     * C# documentation explicitly mention catching the disposed object, as we cannot know in advance if the stream is disposed. */
+                    return false;
                 }
             }
 
@@ -621,8 +620,7 @@ namespace WatsonWebserver.Lite
                 }
                 catch (ObjectDisposedException)
                 {
-                    /* Always check if the stream is disposed, some clients are picky about this and cut the wire in the middle of a request.
-                     * C# documentation explicitly mention catching the disposed object, as we cannot know in advance if the stream is disposed. */
+                    return false;
                 }
             }
 
@@ -631,13 +629,11 @@ namespace WatsonWebserver.Lite
                 try
                 {
                     _Stream.Close();
-                    ResponseSent = true; /* We use the flag at our advantage, so we can effectively check for dead wires, sounds a bit unsafe,
-                                          * at this specific place but we can assume the stream was cut if the closing operation didn't complete. */
+                    ResponseSent = true;
                 }
                 catch (ObjectDisposedException)
                 {
-                    /* Always check if the stream is disposed, some clients are picky about this and cut the wire in the middle of a request.
-                     * C# documentation explicitly mention catching the disposed object, as we cannot know in advance if the stream is disposed. */
+                    return false;
                 }
             }
 
